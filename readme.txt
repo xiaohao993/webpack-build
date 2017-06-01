a54e42b9bb188c13fc4e5d15d1894f6bb844c9fb
vincenwen <vincen.wen@samesamechina.com>
2017-06-01 14:28:18 +0800

    update -- 把手动设置压缩html模版改为动态自动抓取设置;

--- readme.txt
+++ readme.txt
@@ -21,6 +21,110 @@ webpack2.2 中文文档
 
 
 
+=======================
+
+参考项目： https://github.com/AlloyTeam/omi-cli/blob/master/template/app/config/project.js
+
+
+2017-6-1        自动获取html模版
+
+steamer-webpack-utils 
+
+getHtmlEntry
+自动获取 html 文件
+参数 {Object}
+option.srcPath {String} 包含有 html 文件的目录
+option.level {Integer} 0 表示在当前目录寻找，1 表示在下一级目录寻找
+options.keyType {String} 返回 key 值的类型， 默认 folderName (用父文件夹名作 key), 其它值 fileName(用文件名作为 key)
+
+
+const configCustom = {
+	webpack: {
+		html: utils.filterHtmlFileByCmd(utils.getHtmlEntry({
+	        srcPath: path.join(PATHS.source, "pages"),
+	        level: 1
+	    }))
+	},
+	getPlugins: function(){
+		const plugins = [];
+		var self = this;
+		this.webpack.html.forEach(function(page, key) {
+			const order = ['base'];
+			order.push(page.key);
+			//self.webpack['order'].push(page.key);
+			plugins.push(new HtmlWebpackPlugin({
+				filename: page.key + '.html',
+				chunks: ['base', page.key],
+				template: page.path,
+				inject: 'head',
+				chunksSortMode: function(chunk1, chunk2){   // js/css 插入页面的顺序设定
+					var order1 = order.indexOf(chunk1.names[0]);
+					var order2 = order.indexOf(chunk2.names[0]);
+					return order1 - order2;
+				},
+				showErrors: true,       // 是否将错误信息输出到html页面中
+				hash: true 
+			}))
+            // plugins.push(new HtmlResWebpackPlugin({
+            //     mode: "html",
+            //     filename: isProduction ? (config.webpack.path.distWebserver + "/" + page.key + ".html") : page.key + ".html",
+            //     template: page.path,
+            //     favicon: "src/favicon.ico",
+            //     htmlMinify: null,
+            //     entryLog: !key ? true : false,
+            //     cssPublicPath: isProduction ? config.webpack.cssCdn : config.webpack.webserver,
+            //     templateContent: function(tpl) {
+            //         return tpl;
+            //     }
+            // }));
+        }); 
+
+        return plugins;
+	}
+}
+
+
+
+手动建页面(旧的)
+// new HtmlWebpackPlugin({
+// 	filename: 'index.html',
+// 	chunks: ['base', 'index'],
+// 	template: PATHS.source + '/pages/index/index.html',
+// 	inject: 'head',
+// 	chunksSortMode: chunksOrder,
+// 	showErrors: true,       // 是否将错误信息输出到html页面中
+// 	hash: true 
+// }),
+// new HtmlWebpackPlugin({
+// 	filename: 'blog.html',
+// 	chunks: ['base', 'blog'],
+// 	template: PATHS.source + '/pages/blog/blog.html',
+// 	inject: 'head',
+// 	chunksSortMode: chunksOrder,
+// 	showErrors: true,       // 是否将错误信息输出到html页面中
+// 	hash: true 
+// })
+
+
+
+=======================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path'),
 	  HtmlWebpackPlugin = require('html-webpack-plugin'),
 	  ExtractTextPlugin = require('extract-text-webpack-plugin'),
 	  CleanWebpackPlugin = require('clean-webpack-plugin'),
-	  tinypngCompress = require('webpack-tinypng-compress');
+	  tinypngCompress = require('webpack-tinypng-compress'),
+	  utils = require('steamer-webpack-utils');
 
 
 const ENV = process.env.npm_lifecycle_event;
@@ -18,21 +19,70 @@ const extractPlugin = new ExtractTextPlugin({
 	allChunks: true
 });
 
+// ========================= webpack路径与url =========================
+// 项目路径
 const PATHS = {
 	source: path.join(__dirname, 'src'),
 	build: path.join(__dirname, 'build')
 };
 
-const chunksOrder = function(chunk1, chunk2){   // js/css 插入页面的顺序设定
-	var order = ['base', 'index', 'blog'];
-	var order1 = order.indexOf(chunk1.names[0]);
-	var order2 = order.indexOf(chunk2.names[0]);
-	return order1 - order2;
-};
+// const chunksOrder = function(chunk1, chunk2){   // js/css 插入页面的顺序设定
+// 	var order = ['base', 'index', 'blog'];
+// 	var order1 = order.indexOf(chunk1.names[0]);
+// 	var order2 = order.indexOf(chunk2.names[0]);
+// 	return order1 - order2;
+// };
 
+ 
+// 自动获取html模版  配置
+const configCustom = {
+	webpack: {
+		html: utils.filterHtmlFileByCmd(utils.getHtmlEntry({
+	        srcPath: path.join(PATHS.source, "pages"),
+	        level: 1
+	    }))
+	},
+	getPlugins: function(){
+		const plugins = [];
+		var self = this;
+		this.webpack.html.forEach(function(page, key) {
+			const order = ['base'];
+			order.push(page.key);
+			//self.webpack['order'].push(page.key);
+			plugins.push(new HtmlWebpackPlugin({
+				filename: page.key + '.html',
+				chunks: ['base', page.key],
+				template: page.path,
+				inject: 'head',
+				chunksSortMode: function(chunk1, chunk2){   // js/css 插入页面的顺序设定
+					var order1 = order.indexOf(chunk1.names[0]);
+					var order2 = order.indexOf(chunk2.names[0]);
+					return order1 - order2;
+				},
+				showErrors: true,       // 是否将错误信息输出到html页面中
+				hash: true 
+			}))
+            // plugins.push(new HtmlResWebpackPlugin({
+            //     mode: "html",
+            //     filename: isProduction ? (config.webpack.path.distWebserver + "/" + page.key + ".html") : page.key + ".html",
+            //     template: page.path,
+            //     favicon: "src/favicon.ico",
+            //     htmlMinify: null,
+            //     entryLog: !key ? true : false,
+            //     cssPublicPath: isProduction ? config.webpack.cssCdn : config.webpack.webserver,
+            //     templateContent: function(tpl) {
+            //         return tpl;
+            //     }
+            // }));
+        }); 
+
+        return plugins;
+	}
+}
+
+// console.log(configCustom.getPlugins());
 
 const config = {
-	devtool: "source-map",  //配置生成Source Maps，选择合适的选项
 	entry: {
 		'base': PATHS.source + '/assets/js/base.js',          // 主体共享js
 		'index': PATHS.source + '/pages/index/index.js',
@@ -49,24 +99,24 @@ const config = {
 			jQuery: 'jquery'
 		}),
 		extractPlugin,
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			chunks: ['base', 'index'],
-			template: PATHS.source + '/pages/index/index.html',
-			inject: 'head',
-			chunksSortMode: chunksOrder,
-			showErrors: true,       // 是否将错误信息输出到html页面中
-			hash: true 
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'blog.html',
-			chunks: ['base', 'blog'],
-			template: PATHS.source + '/pages/blog/blog.html',
-			inject: 'head',
-			chunksSortMode: chunksOrder,
-			showErrors: true,       // 是否将错误信息输出到html页面中
-			hash: true 
-		}),
+		// new HtmlWebpackPlugin({
+		// 	filename: 'index.html',
+		// 	chunks: ['base', 'index'],
+		// 	template: PATHS.source + '/pages/index/index.html',
+		// 	inject: 'head',
+		// 	chunksSortMode: chunksOrder,
+		// 	showErrors: true,       // 是否将错误信息输出到html页面中
+		// 	hash: true 
+		// }),
+		// new HtmlWebpackPlugin({
+		// 	filename: 'blog.html',
+		// 	chunks: ['base', 'blog'],
+		// 	template: PATHS.source + '/pages/blog/blog.html',
+		// 	inject: 'head',
+		// 	chunksSortMode: chunksOrder,
+		// 	showErrors: true,       // 是否将错误信息输出到html页面中
+		// 	hash: true 
+		// })
 	],
 	module: {
 		rules: [
@@ -123,7 +173,7 @@ const config = {
 					{
 						loader: "url-loader",
 						options: {
-							limit: 10000,
+							limit: 1000,
 							name: '[name].[ext]',
 							outputPath: 'img/',
 							publicPath: '/build/'
@@ -142,16 +192,18 @@ const config = {
 
 // 编译环境判断
 if(ENV === 'build'||ENV === 'build:prod'){
+	config.devtool = "source-map";  //配置生成Source Maps，选择合适的选项
 	config.plugins = config.plugins.concat([
 		new tinypngCompress({
 			key: tpKey,
 			relativePath: PATHS.build + '/img'  //is relative path to output.puth 
 		}),
 		new CleanWebpackPlugin([PATHS.build])
-	]);
+	], configCustom.getPlugins());
 	//console.log(config);
-}
+}else{
 
+}
 
 
 
